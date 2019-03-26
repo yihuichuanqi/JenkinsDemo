73bfc7f9712bf646e83170846e29c3860e1220cc
lyn <>
2019-03-26 13:55:27 +0800

    first commit

---
+++ JenkinsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenkinsDemo
+//
+//  Created by apple on 2019/3/26.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JenkinsDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  JenkinsDemo
+//
+//  Created by apple on 2019/3/26.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
