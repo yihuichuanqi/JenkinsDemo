d0bebdb7f5c7a0957ac8da50616fcec8f9675a6c
shichuanlei <1206714254@qq.com>
2023-03-11 17:36:18 +0800

    主分支 新增Model

---
+++ JenkinsDemo/MasterModel.h
@@ -0,0 +1,17 @@
+//
+//  MasterModel.h
+//  JenkinsDemo
+//
+//  Created by scl on 2023/3/11.
+//  Copyright © 2023 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MasterModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JenkinsDemo/MasterModel.m
@@ -0,0 +1,13 @@
+//
+//  MasterModel.m
+//  JenkinsDemo
+//
+//  Created by scl on 2023/3/11.
+//  Copyright © 2023 apple. All rights reserved.
+//
+
+#import "MasterModel.h"
+
+@implementation MasterModel
+
+@end
