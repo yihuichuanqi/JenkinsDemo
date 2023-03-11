c5472efda2695882763e46d275c180ad8effe09e
shichuanlei <1206714254@qq.com>
2023-03-11 17:42:23 +0800

    分支2 新增view

---
+++ JenkinsDemo/KLView2.m
@@ -0,0 +1,21 @@
+//
+//  KLView2.m
+//  JenkinsDemo
+//
+//  Created by scl on 2023/3/11.
+//  Copyright © 2023 apple. All rights reserved.
+//
+
+#import "KLView2.h"
+
+@implementation KLView2
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
